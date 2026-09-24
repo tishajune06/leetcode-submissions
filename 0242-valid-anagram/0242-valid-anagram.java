98165229fa1b9e1e1c4a672f7020aef61952090e
Tisha Jindal <jindaltisha14@gmail.com>
2026-09-24 11:51:35 +0530

    Time: 17 ms (16.57%), Space: 46.6 MB (15.74%) - LeetHub-Neo

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        for(int i = 0; i<s.length(); i++){
+            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0)+1);
+        }
+        HashMap<Character,Integer> map2 = new HashMap<>();
+        for(int i = 0; i<t.length(); i++){
+            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0)+1);
+        }
+        return map1.equals(map2);
+    }
+}
